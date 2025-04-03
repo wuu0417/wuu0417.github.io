00834460a9a912d11929ac0223eb79ace51380ba
Wu You <120346213+wuu0417@users.noreply.github.com>
2025-04-04 07:50:45 +0800

    Delete requirements/nowdate.js

--- requirements/nowdate.js
+++
@@ -1,8 +0,0 @@
-function updateTime() {
-            const now = new Date();
-            const timeString = now.toLocaleTimeString();
-            document.getElementById("time").textContent = timeString;
-        }
-        
-        setInterval(updateTime, 1000); // 获取当前时间，每秒更新一次
-        window.onload = updateTime; // 页面加载时立即更新一次
